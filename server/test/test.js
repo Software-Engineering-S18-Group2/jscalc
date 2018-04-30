20f2296d8636a7b7392c3885e3fe428334dccdaf
CHico-Lee <leechiho22@hotmail.com>
2018-04-30 10:05:30 -0400

    initial sample unit test with mocha

---
+++ server/test/test.js
@@ -0,0 +1,14 @@
+// This is a sample unit test.
+var assert = require('chai').assert;
+
+describe('math module', function(){
+    it('should add numbers', function () {
+        assert.equal((1+1), '2');
+        assert.strictEqual(127 + 319, 446);
+    });
+
+    it('should sub numbers', function () {
+        assert.equal((22-1), '21');
+        assert.strictEqual(127 - 7, 120);
+    });
+});
