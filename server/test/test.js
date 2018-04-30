96b78e86355d13c8da264a6a50215652667114bc
CHico-Lee <leechiho22@hotmail.com>
2018-04-30 15:20:40 -0400

    test content for http request, removed commented code

--- server/test/test.js
+++ server/test/test.js
@@ -17,10 +17,7 @@ describe('server response', function () {
     // test http request
     it('should return 200', function (done) {
         request.get('http://localhost:3000', function (err, res, body){
-            //  assert.equals(res.statusCode,200);
-            //  console.log(res);
             expect(res.statusCode).to.equal(200);
-            //expect(res.body).to.equal('wrong header');
             done();
         });
     });
@@ -28,12 +25,17 @@ describe('server response', function () {
     // http request fail
     it('should return false', function (done) {
         request.get('http://localhost:3000', function (err, res, body){
-            //  assert.equals(res.statusCode,200);
-            //  console.log(res);
             assert.notEqual(res.body,"akjsdhkja");
-            //expect(res.body).to.equal('wrong header');
             done();
         });
     });
 
+    // test content for http request
+    it('should return true for terms', function (done) {
+        request.get('http://localhost:3000/terms', function (err, res, body){
+            var body = res.body;
+            assert.isTrue(body.includes("terms") );
+            done();
+        });
+    });
 });
