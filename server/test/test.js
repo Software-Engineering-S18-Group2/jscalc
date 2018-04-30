8f340ef9735023c7e1b92ec674f10dd42e4aa42c
CHico-Lee <leechiho22@hotmail.com>
2018-04-30 14:41:20 -0400

    add testing for http request fail, define var monfoose and server

--- server/test/test.js
+++ server/test/test.js
@@ -1,12 +1,14 @@
 //During the test the env variable is set to test
 var assert = require('chai').assert;
 
+var mongoose = require("mongoose");
 var expect = require('Chai').expect;
 var request = require('request');
 
 //Require the dev-dependencies
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var server = require('../app');
 
 chai.use(chaiHttp);
 //Our parent block
@@ -18,6 +20,17 @@ describe('server response', function () {
             //  assert.equals(res.statusCode,200);
             //  console.log(res);
             expect(res.statusCode).to.equal(200);
+            //expect(res.body).to.equal('wrong header');
+            done();
+        });
+    });
+
+    // http request fail
+    it('should return false', function (done) {
+        request.get('http://localhost:3000', function (err, res, body){
+            //  assert.equals(res.statusCode,200);
+            //  console.log(res);
+            assert.notEqual(res.body,"akjsdhkja");
             //expect(res.body).to.equal('wrong header');
             done();
         });
