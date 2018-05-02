b85612823d19436a0dd2a69c1b5b905457483713
rohith28 <rohith28@github.com>
2018-05-02 12:03:08 -0400

    Deleting clutter comments

--- server/app.js
+++ server/app.js
@@ -239,12 +239,9 @@ var maybePrerender = function(req, res, next) {
  */
 
 app.use(function (req, res, next) {
-// res.header("Access-Control-Allow-Origin", "*");
-// Asgn 6 - replace above line with this
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-// Asgn - 6
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
